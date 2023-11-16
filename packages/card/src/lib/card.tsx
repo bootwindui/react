24ca70fab02431120b182dc3b2118b74dd2edced
zuramai <saugi.dev@gmail.com>
2023-11-16 07:59:38 +0700

    feat: test card component

---
+++ packages/card/src/lib/card.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable-next-line */
+export interface CardProps {}
+
+export function Card(props: CardProps) {
+  return (
+    <div className="p-5 shadow-lg rounded-lg">
+      <h1>Welcome to Card!</h1>
+    </div>
+  );
+}
+
+export default Card;

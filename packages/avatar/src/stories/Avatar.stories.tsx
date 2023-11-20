038cc0f5319fb2c4b157b39f7813379a59d01d69
zuramai <saugi.dev@gmail.com>
2023-11-20 10:14:09 +0700

    refactor: avatar autodocs

--- packages/avatar/src/stories/Avatar.stories.tsx
+++ packages/avatar/src/stories/Avatar.stories.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Meta } from '@storybook/react';
+import { Meta, StoryObj } from '@storybook/react';
 
 import { Avatar, AvatarProps } from '../index';
 import Title from '@bootwind/title';
@@ -7,6 +7,7 @@ import Title from '@bootwind/title';
 export default {
   title: 'Components/Avatar',
   component: Avatar,
+  tags: ['autodocs'],
   argTypes: {
     rounded: {
       options: ['none', 'sm', 'md', 'lg', 'full'],
@@ -17,30 +18,19 @@ export default {
       control: { type: 'radio' },
     },
     showPlaceholder: {
+      description: 'Show avatar placeholder if `src` is null',
       control: 'boolean',
     },
   },
 } as Meta;
+type Story = StoryObj<AvatarProps>;
 
-export const Avatars = (args: AvatarProps) => (
-  <>
-    <Title
-      title="Avatars"
-      description="The Avatars component in the Bootwind Design System lets you personalize user profiles and identities with ease and style. Customize the avatar's size, shape, and appearance to create unique and visually appealing representations, enhancing the user experience and adding a touch of personality to your application or website."
-    />
-
-    <div className="flex space-x-4 px-8">
-      <Avatar
-        src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-        {...args}
-        alt="User 1"
-      />
-      <Avatar
-        src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-        {...args}
-        alt="User 2"
-      />
-      <Avatar showPlaceholder={true} {...args} alt="User 2" />
-    </div>
-  </>
-);
+export const Basic: Story = {
+  args: {
+    rounded: 'full',
+    notificationDot: 'gray',
+    alt: 'User 1 Avatar',
+    showPlaceholder: false,
+    src: "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+  }
+}

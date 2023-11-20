9b7cb5be28d7f29851490a097169d780d82a0e1f
zuramai <saugi.dev@gmail.com>
2023-11-20 15:24:09 +0700

    feat: pagination example

--- packages/ui/src/stories/examples/TablePagination.stories.tsx
+++ packages/ui/src/stories/examples/TablePagination.stories.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { Meta } from '@storybook/react';
 
 import Title from '@bootwind/title';
-import { Pagination } from '../index';
+import { Pagination } from '@bootwind/pagination';
 
 const userData = Array.from({ length: 50 }, (_, index) => ({
   id: index + 1,
@@ -17,129 +17,11 @@ const columns = [
 ];
 
 export default {
-  title: 'Components/Pagination',
+  title: 'Examples/TablePagination',
   component: Pagination,
-  tags: ['autodocs'],
   argTypes: {},
 } as Meta;
 
-const TemplateDefault = () => {
-  const [currentPage, setCurrentPage] = useState(1);
-  const resultsPerPage = 10;
-
-  const onPageChange = (page: number) => {
-    setCurrentPage(page);
-  };
-
-  const startIndex = (currentPage - 1) * resultsPerPage;
-  const endIndex = currentPage * resultsPerPage;
-  const paginatedData = userData.slice(startIndex, endIndex);
-
-  return (
-    <>
-      <div className="px-4 sm:px-6 lg:px-8">
-        <div className="sm:flex sm:items-center">
-          <div className="sm:flex-auto">
-            <h1 className="text-xl font-semibold text-gray-900">Users</h1>
-            <p className="mt-2 text-sm text-gray-700">
-              A list of all the users in your account including their name, title,
-              email and role.
-            </p>
-          </div>
-          <div className="mt-4 sm:mt-0 sm:ml-16 sm:flex-none">
-            <button
-              type="button"
-              className="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto"
-            >
-              Add user
-            </button>
-          </div>
-        </div>
-        <div className="mt-8 flex flex-col">
-          <div className="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
-            <div className="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
-              <div className="overflow-hidden shadow ring-1 ring-black ring-opacity-5 md:rounded-lg">
-                <table className="min-w-full divide-y divide-gray-300">
-                  <thead className="bg-gray-50">
-                    <tr>
-                      <th
-                        scope="col"
-                        className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6"
-                      >
-                        Number
-                      </th>
-                      <th
-                        scope="col"
-                        className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
-                      >
-                        Name
-                      </th>
-                      <th
-                        scope="col"
-                        className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
-                      >
-                        Email
-                      </th>
-                      <th
-                        scope="col"
-                        className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
-                      >
-                        Role
-                      </th>
-                      <th
-                        scope="col"
-                        className="relative py-3.5 pl-3 pr-4 sm:pr-6"
-                      >
-                        <span className="sr-only">Edit</span>
-                      </th>
-                    </tr>
-                  </thead>
-                  <tbody className="divide-y divide-gray-200 bg-white">
-                    {paginatedData.map(person => (
-                      <tr key={person.email}>
-                        <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6">
-                          {person.id}
-                        </td>
-                        <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                          {person.name}
-                        </td>
-                        <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                          {person.email}
-                        </td>
-                        <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                          Member
-                        </td>
-                        <td className="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6">
-                          <a
-                            href="#"
-                            className="text-indigo-600 hover:text-indigo-900"
-                          >
-                            Edit<span className="sr-only">, {person.name}</span>
-                          </a>
-                        </td>
-                      </tr>
-                    ))}
-                  </tbody>
-                </table>
-                <div className="px-4">
-                  <Pagination
-                    type="card"
-                    currentPage={currentPage}
-                    totalResults={userData.length}
-                    resultsPerPage={resultsPerPage}
-                    onPrevClick={() => onPageChange(currentPage - 1)}
-                    onNextClick={() => onPageChange(currentPage + 1)}
-                    onPageClick={page => onPageChange(page)}
-                  />
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </>
-  );
-};
 
 const TemplateSimple = () => {
   const [currentPage, setCurrentPage] = useState(1);
@@ -155,10 +37,6 @@ const TemplateSimple = () => {
 
   return (
     <>
-      <Title
-        title="Pagination"
-        description="The Pagination component in the Bootwind Design System allows you to navigate through a collection of items or pages in a structured and organized manner. With different types available, including Pagination Medium, Pagination Small, and Pagination Text, you can choose the style and functionality that best suits your design requirements."
-      />
 
       <div className="px-4 sm:px-6 lg:px-8">
         <div className="sm:flex sm:items-center">
@@ -278,12 +156,6 @@ const TemplateCentered = () => {
 
   return (
     <>
-
-      <Title
-        title="Pagination"
-        description="The Pagination component in the Bootwind Design System allows you to navigate through a collection of items or pages in a structured and organized manner. With different types available, including Pagination Medium, Pagination Small, and Pagination Text, you can choose the style and functionality that best suits your design requirements."
-      />
-
       <div className="px-4 sm:px-6 lg:px-8">
         <div className="sm:flex sm:items-center">
           <div className="sm:flex-auto">
@@ -387,7 +259,5 @@ const TemplateCentered = () => {
     </>
   );
 };
-
-export const Default = TemplateDefault.bind({});
 export const Simple = TemplateSimple.bind({});
 export const Centered = TemplateCentered.bind({});

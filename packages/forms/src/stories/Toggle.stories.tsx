08746a1a3d43ccb32bfcf8070ddd237d11fc3fbd
zuramai <saugi.dev@gmail.com>
2023-11-20 15:06:42 +0700

    refactor: toggle component autodoc

--- packages/forms/src/stories/Toggle.stories.tsx
+++ packages/forms/src/stories/Toggle.stories.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Meta } from '@storybook/react';
+import { Meta, StoryObj } from '@storybook/react';
 
 import Title from '@bootwind/title';
 import { Toggle, ToggleProps } from '../index';
@@ -7,46 +7,24 @@ import { FaMoon, FaSun } from 'react-icons/fa6';
 
 export default {
   title: 'Components/Forms/Toggle',
+  tags: ['autodocs'],
   component: Toggle,
   argTypes: {
     enabled: { control: 'boolean' },
   },
 } as Meta;
+type Story = StoryObj<ToggleProps>;
 
-export const ToggleDark = (args: ToggleProps) => (
-  <>
 
-    <Title
-      title="Toggle"
-      description="Toggle"
-    />
-
-    <div className="flex flex-col space-y-4">
-      <Toggle
-        onToggle={isEnabled => console.log('Switch toggled:', isEnabled)}
-        color="primary"
-        iconOn={
-          <FaSun className="text-primary h-3 w-3 mt-1 mx-auto justify-center items-center" />
-        }
-        iconOff={<FaMoon className="text-gray-500 h-3 w-3 mx-auto mt-1" />}
-      />
-      <Toggle
-        onToggle={isEnabled => console.log('Switch toggled:', isEnabled)}
-        color="secondary"
-        iconOn={
-          <FaSun className="text-black h-3 w-3 mt-1 mx-auto justify-center items-center" />
-        }
-        iconOff={<FaMoon className="text-gray-500 h-3 w-3 mx-auto mt-1" />}
-      />
-      <Toggle
-        onToggle={isEnabled => console.log('Switch toggled:', isEnabled)}
-        color="primary"
-      />
-      <Toggle
-        onToggle={isEnabled => console.log('Switch toggled:', isEnabled)}
-        color="secondary"
-      />
-
-    </div>
-  </>
-);
+export const Basic: Story = {
+  args: {
+    color: 'primary'
+  }
+}
+export const WithIcon: Story = {
+  args: {
+    color: 'primary',
+    iconOn: (<FaSun className="text-primary h-3 w-3 mt-1 mx-auto justify-center items-center" />),
+    iconOff: (<FaMoon className="text-gray-500 h-3 w-3 mx-auto mt-1" />)
+  }
+}

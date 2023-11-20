2813380a7f833a6efe8fbaad0e8430fc3e83a550
zuramai <saugi.dev@gmail.com>
2023-11-20 15:02:02 +0700

    refactor: checkbox autodoc

--- packages/forms/src/stories/Checkbox.stories.tsx
+++ packages/forms/src/stories/Checkbox.stories.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Meta } from '@storybook/react';
+import { Meta, StoryObj } from '@storybook/react';
 
 import Title from '@bootwind/title';
 import { CheckboxProps, Checkbox } from '../index';
@@ -7,18 +7,24 @@ import { CheckboxProps, Checkbox } from '../index';
 export default {
   title: 'Components/Forms/Checkbox',
   component: Checkbox,
+  tags: ['autodocs'],
   argTypes: {
     isRounded: { control: 'boolean' },
     indeterminate: { control: 'boolean' },
   },
 } as Meta;
+type Story = StoryObj<CheckboxProps>;
 
+export const Basic: Story = {
+  args: {
+    id: 'myRandomCheckboxId',
+    isRounded: true,
+    label: 'Basic Checkbox',
+    indeterminate: false
+  }
+};
 export const Variants = (args: CheckboxProps) => (
   <>
-    <Title
-      title="Checkbox"
-      description="The Checkbox component in the Bootwind Design System allows users to make selections from a set of options or toggle a single option on or off. With customizable options for labels, sizes, types, statuses, and states, you can create versatile checkboxes that fit your design needs."
-    />
     <div className="flex flex-col space-y-4">
       <div>
         <Checkbox
@@ -34,21 +40,9 @@ export const Variants = (args: CheckboxProps) => (
   </>
 );
 
-export const IndeterminateState = (args: CheckboxProps) => (
-  <>
-    <Title
-      title="Checkbox"
-      description="The Checkbox component in the Bootwind Design System allows users to make selections from a set of options or toggle a single option on or off. With customizable options for labels, sizes, types, statuses, and states, you can create versatile checkboxes that fit your design needs."
-    />
-    <div className="flex flex-col space-y-4">
-      <div>
-        <Checkbox
-          isRounded={false}
-          id="indeterminateCheckbox"
-          label="Indeterminate Checkbox"
-          indeterminate={true}
-        />
-      </div>
-    </div>
-  </>
-);
+export const IndeterminateState: Story = {
+  args: {
+    label: 'Basic Checkbox',
+    indeterminate: true
+  }
+};

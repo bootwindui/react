0e323f2aa568f907db297b84bff283a27d0aaf2f
zuramai <saugi.dev@gmail.com>
2023-11-20 11:56:37 +0700

    feat(component): left and right icon in button component

--- packages/button/src/lib/button.tsx
+++ packages/button/src/lib/button.tsx
@@ -1,28 +1,28 @@
 import React, { ReactElement } from 'react';
 import { ReactNode } from 'react';
 
-type ButtonVariant = 'primary' | 'secondary' | 'ghost';
+type ButtonVariant = 'primary' | 'secondary' | 'text-only';
 type ButtonSize = 'sm' | 'md' | 'lg' | 'xl';
 
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   variant?: ButtonVariant;
   size?: ButtonSize;
-  hasArrow?: boolean;
-  icon?: ReactNode;
+  leftIcon?: ReactNode;
+  rightIcon?: ReactNode;
   children?: ReactNode
 }
 
 export const Button = ({
   variant = 'primary',
   size = 'md',
-  hasArrow = false,
-  icon,
+  leftIcon,
+  rightIcon,
   children,
   ...props
 }: ButtonProps) => {
   const baseStyles =
-    'inline-flex items-center rounded-lg border border-transparent font-semibold';
+    'inline-flex items-center rounded-lg border border-transparent font-semibold  disabled:text-[#CED2D6]';
 
   const sizeStyles = {
     sm: 'px-3 py-2 text-sm',
@@ -33,10 +33,15 @@ export const Button = ({
 
   const colorStyles = {
     primary:
-      'bg-primary text-white shadow-sm hover:bg-[#525CD9] hover:outline-none hover:ring-1 hover:ring-gray-200 focus:outline-none focus:ring-4 focus:ring-gray-200',
+      `bg-primary text-white shadow-sm hover:bg-[#525CD9] border border-gray-50
+      enabled:hover:outline-none enabled:hover:ring-1 enabled:hover:ring-gray-200 
+      focus:outline-none focus:ring-4 focus:ring-gray-200
+      disabled:bg-[#F6F7F9] disabled:border-gray-300`,
     secondary:
-      'border border-slate-200 bg-white text-black shadow-sm hover:border-gray-500 hover:bg-[#F6F7F9] focus:outline-none focus:ring-2 focus:ring-gray-300',
-    ghost: 'text-black',
+      `border-slate-300 border text-black shadow-sm 
+      enabled:hover:border-gray-500 enabled:hover:bg-[#F6F7F9]
+      focus:outline-none focus:ring-2 focus:ring-gray-300`,
+    'text-only': 'text-black',
   };
 
   const buttonStyles = `${baseStyles} ${sizeStyles[size]} ${colorStyles[variant]}`;
@@ -44,9 +49,9 @@ export const Button = ({
   return (
     <>
     <button type="button" className={buttonStyles} {...props}>
-      {icon && <span className="mr-2">{icon}</span>}
+      {leftIcon && <span className="mr-2">{leftIcon}</span>}
       {children}
-      {hasArrow && <span className="ml-2">→</span>}
+      {rightIcon && <span className="ml-2">{rightIcon}</span>}
     </button>
     </>
   );

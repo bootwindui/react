24dfa3c02c519d563a685f974720d19536dcc7e4
syauqi <syaokay@gmail.com>
2024-01-23 11:48:06 +0000

    Deploying to gh-pages from @ bootwindproject/bootwindui@b40dba0bca8e17f0493556d48cc1f3034c2803d3 🚀

--- assets/preview-8ff3514e.js
+++
@@ -1,2 +0,0 @@
-import{_ as t}from"./iframe-4b37f38b.js";import"../sb-preview/runtime.js";var _={docs:{renderer:async()=>{let{DocsRenderer:r}=await t(()=>import("./DocsRenderer-NNNQARDV-8e5bb8c5.js").then(e=>e.D),["./DocsRenderer-NNNQARDV-8e5bb8c5.js","./iframe-4b37f38b.js","./index-76fb7be0.js","./_commonjsHelpers-de833af9.js","./react-18-988a5df2.js","./index-d3ea75b5.js","./_getPrototype-1e53b583.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{_ as parameters};
-//# sourceMappingURL=preview-8ff3514e.js.map
---
+++ assets/preview-e15791a4.js
@@ -0,0 +1,2 @@
+import{_ as t}from"./iframe-8bd3c127.js";import"../sb-preview/runtime.js";var _={docs:{renderer:async()=>{let{DocsRenderer:r}=await t(()=>import("./DocsRenderer-NNNQARDV-995bd8c1.js").then(e=>e.D),["./DocsRenderer-NNNQARDV-995bd8c1.js","./iframe-8bd3c127.js","./index-76fb7be0.js","./_commonjsHelpers-de833af9.js","./react-18-988a5df2.js","./index-d3ea75b5.js","./_getPrototype-1e53b583.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{_ as parameters};
+//# sourceMappingURL=preview-e15791a4.js.map
